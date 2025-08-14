64ebc1172c3a15da7b6b804f000ea75eef584031
Lucky <126210746+luckyshotpictures@users.noreply.github.com>
2025-08-14 17:16:36 +0200

    directions, reject ne/sw/etc

---
+++ Content.Shared/Signs/DynamicSignsDir.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Maths;
+
+namespace Content.Shared.Signs;
+
+public static class DynamicSignsDir
+{
+    public static readonly Direction[] Cardinals =
+    {
+        Direction.North,
+        Direction.South,
+        Direction.East,
+        Direction.West,
+    };
+
+    public static bool IsCardinal(Direction d) =>
+        d is Direction.North or  Direction.South or Direction.East or Direction.West;
+}
